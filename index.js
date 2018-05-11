40d50eea5c2f711a596674a5eb72675ad9f3b8d2
teub0005 <31710545+teub0005@users.noreply.github.com>
2018-05-11 13:07:15 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/public/index.html');
 });
-
+app.get('/aboutus.html', function(req, res){
+	console.log("about us");
+	app.use(express.static(__dirname + '/public/images'));
+	res.sendFile(__dirname + '/public/aboutus.html');
+});
 io.on('connection', function(socket){
 	console.log('a user connected');
 	socket.on('chat message', function(msg){
